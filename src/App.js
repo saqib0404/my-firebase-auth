3ca5cf1d515ad17d40f8f1ef9a78be800cb478e0
Saqib Ahmad <you@example.com>
2022-10-16 19:34:14 +0600

    github auth added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
 import './App.css';
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut, TwitterAuthProvider } from 'firebase/auth'
+import app from './firebase/firebase.init';
+import { useState } from 'react';
+
+const auth = getAuth(app)
 
 function App() {
+  const [user, setUser] = useState({})
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+  const twitterProvider = new TwitterAuthProvider()
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        setUser(result.user);
+        console.log(result.user);
+      })
+      .catch(err => console.error('error ', err))
+  }
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then(result => setUser(result.user))
+      .catch(err => console.error('error: ', err))
+  }
+
+  const handleTwitterSignIn = () => {
+    signInWithPopup(auth, twitterProvider)
+      .then(result => setUser(result.user))
+      .catch(err => console.error('error: ', err))
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({})
+      })
+  }
+
   return (
     <div className="App">
-      
+      {
+        user.displayName ?
+          <button onClick={handleSignOut}>Sign Out</button>
+          :
+          <>
+            <button onClick={handleGoogleSignIn}>Google Sign In</button>
+            <button onClick={handleGithubSignIn}>Github Sign In</button>
+            <button onClick={handleTwitterSignIn}>Twitter Sign In</button>
+          </>
+      }
+      {user.displayName && <div>
+        <h4>Name: {user.displayName}</h4>
+        <p>Email: {user.email}</p>
+        <img src={user.photoURL} alt="" />
+      </div>}
     </div>
   );
 }
